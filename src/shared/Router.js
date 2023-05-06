a3cf4e2ef2685b1a807d09a07dff575aad4063c6
Leezero3 <fuddl4525@naver.com>
2023-05-06 23:28:06 +0900

    router

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,16 +1,19 @@
 // import { formToJSON } from "axios";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Join, Login, NewPost, StudyGroupBoard, DetailPost } from "pages/index";
+import { Join, Login, NewPost, StudyGroupBoard } from "../pages";
+import { GlobalStyle } from "common/feature";
+import { DetailPost } from "pages/index";
 
 const Router = () => {
     return (
         <BrowserRouter>
+            <GlobalStyle />
             <Routes>
                 <Route path="/" element={<StudyGroupBoard />}></Route>
-                <Route path="/NewPost" element={<NewPost />}></Route>
-                <Route path="/Login" element={<Login />}></Route>
-                <Route path="/Join" element={<Join />}></Route>
-                <Route path="/DetailPost" element={<DetailPost />}></Route>
+                <Route path="/new-post" element={<NewPost />}></Route>
+                <Route path="/login" element={<Login />}></Route>
+                <Route path="/join" element={<Join />}></Route>
+                <Route path="/detail-post" element={<DetailPost />}></Route>
             </Routes>
         </BrowserRouter>
     );
